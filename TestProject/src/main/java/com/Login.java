173ec8f4c82fe3bd71ce3a6dfd8ec6bc9fea8951
MyMadSoul <MyMadSoul@192.168.0.102>
2025-11-25 06:29:13 +0700

    created first servlet

---
+++ TestProject/src/main/java/com/Login.java
@@ -0,0 +1,27 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 
+		response.setContentType("text/html"); 
+		PrintWriter out = response.getWriter(); 
+		out.println("Hello World!!");
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response); 
+	}
+
+}
